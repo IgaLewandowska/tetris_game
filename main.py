979f4e4d94082dbf3cd41ab1c401308eb6b3224b
IgaLewandowska <igalewandowska98@wp.pl>
2022-02-03 16:46:47 +0100

    1.1

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import pygame, sys, os
 from pygame.locals import *
 pygame.init()
-window = pygame.display.set_mode((550, 400))
-
+window = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Tetris Game")
+art = pygame.image.load("art.png")
+screen = pygame.display.get_surface()
+screen.blit(art, (50,20))
+pygame.display.flip()
+pygame.mouse.set_cursor(pygame.cursors.tri_left)
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+pygame.quit()
+
+#pygame.FULLSCREEN
+
+
+
+def update():
+    art.left += 2
+    art.top += 2
+    if art.top > 400:
+        art.pos = (0,0)
 
 def input(events):
     for event in events:
@@ -11,6 +31,8 @@ def input(events):
             sys.exit(0)
         else:
             print(event)
+
+
 
 while True:
     input(pygame.event.get(()))
