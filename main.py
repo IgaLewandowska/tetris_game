e0e2152934dcff0ab23f894ee0028aa0b4e22469
IgaLewandowska <igalewandowska98@wp.pl>
2022-02-17 16:58:50 +0100

    1.5

--- main.py
+++ main.py
@@ -1,51 +1,88 @@
 import pygame, sys, os
 from pygame.locals import *
-pygame.init()
+
 width = 680
 height = 480
-screen = pygame.display.set_mode((width, height), 0, 0)
-pygame.display.set_caption("Tetris Game")
-pygame.mouse.set_cursor(pygame.cursors.tri_left)
-
 x = 0
 y = 0
 w = 20
 h = 20
-step = 20
+step = 50
 white = (255, 255, 255)
 black = (0, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 180)
 red = (255, 0, 0)
-screen.fill(white)
-art = pygame.image.load("art-2.png")
+
 artx = 160
 arty = 180
-screen.blit(art, (artx, arty))
-pygame.display.flip()
+#screen.blit(art, (artx, arty))
+#pygame.display.flip()
 screen2 = pygame.display.get_surface()
 
+#screen = {}
+screen = pygame.display.set_mode((width, height))
+#screen = {'surf': False}
 
 
-a = 5
+def main_tetris():
+    pygame.init()
+    pygame.display.set_caption("Tetris Game")
+    pygame.mouse.set_cursor(pygame.cursors.tri_left)
+    art = pygame.image.load("art-2.png")
+    screen.blit(art, (artx, arty))
+    pygame.display.flip()
+    while True:
+        checkEvents()
+        screen.fill(white)
+        drawRect()
+        pygame.display.flip()
+
+
+
+def drawRect():
+    pygame.draw.line(
+        screen, black,
+        (0 + step, 0 + step),
+        (width - step, 0 + step), 2)
+    pygame.draw.line(
+        screen, black,
+        (0 + step, height - step),
+        (width - step, height -step), 2)
+    pygame.draw.line(
+        screen, black,
+        (0 + step, 0 + step),
+        (0 + step, height - step), 2)
+    pygame.draw.line(
+        screen, black,
+        (width - step, 0 + step),
+        (width - step, height - step), 2)
+
+
+
+
+
+
+
+'''a = 5
 b = 5
 running = 1
-'''
+
 while running:
     a += 1
     b += 1
     screen2.blit(art, (a, b))
-    pygame.display.flip()
-'''
+    pygame.display.flip()'''
 
 
+# Podzielic okno na siatke i na niej rysowac tablicą? figury
 
-pygame.draw.polygon(screen, green, ((20, 20), (20, 100), (70, 100), (70, 70), (50, 70), (50, 20), (20, 20)))
+#pygame.draw.polygon(screen, green, ((20, 20), (20, 100), (70, 100), (70, 70), (50, 70), (50, 20), (20, 20)))
 #pygame.draw.polygon(screen, green, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
 #pygame.draw.rect(screen, red, (200, 150, 100, 50))
-pygame.display.flip()
+#pygame.display.flip()
 
-while True:
+'''while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -60,8 +97,8 @@ while True:
     if keys[pygame.K_DOWN]:
         y += step
 
-    pygame.draw.rect(screen, white, (x, y, w, h))
-    pygame.display.flip()
+    #pygame.draw.rect(screen, white, (x, y, w, h))
+    #pygame.display.flip()
 
 
 if direction == "right":
@@ -84,7 +121,7 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-pygame.quit()
+pygame.quit()'''
 
 #rectangle = pygame.Rect(24, 24, 72, 48)
 #screen.fill([0,0,0])
@@ -98,13 +135,21 @@ pygame.quit()
     if art.top > 400:
         art.pos = (0,0)'''
 
-def input(events):
+'''def input(events):
     for event in events:
         if event.type() == QUIT:
             sys.exit(0)
         else:
-            print(event)
+            print(event)'''
 
+def checkEvents():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == KEYDOWN and event.key == K_q:
+            pygame.quit()
+            sys.exit()
 
-while True:
-    input(pygame.event.get(()))
+#while True:
+#    input(pygame.event.get(()))
+main_tetris()
