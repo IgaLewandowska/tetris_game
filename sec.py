70d123351ece8c26d6fa969afac4af113d3348a4
IgaLewandowska <igalewandowska98@wp.pl>
2022-02-20 22:03:50 +0100

    1.1

--- sec.py
+++ sec.py
@@ -3,37 +3,37 @@ import pygame
 import numpy as np
 from pygame.locals import KEYDOWN, K_q
 
-screensize = width, height = 1000, 1000
+screensize = width, height = 1000, 800
 black = (0, 0, 0)
 grey = (160, 160, 160)
+white = (250, 250, 250)
+blue = (0, 0, 180)
 
-cellmap = np.random.randint(2, size = (6, 6))
-print(cellmap)
+#cellmap = np.random.randint(2, size=(15, 15))
+#print(cellmap)
 
-screen = pygame.display.set_mode(screensize)
-gridWH = 400
-gridOrigin = (200, 100)
-gridCell = cellmap.shape[0]
+tab = []
+for x in range(10):
+    add = [0] * 10
+    tab.append(add)
+print(tab)
+
+screen = pygame.display.set_mode(screensize, pygame.RESIZABLE)
+gridWH = 500
+gridOrigin = (50, 50)
+#gridCell = cellmap.shape[0]
 
 
 def main():
     pygame.init()
+    pygame.display.set_caption('TETRIS')
     while True:
         checkEvents()
         screen.fill(grey)
         drawSquareGrid(
-            gridOrigin, gridWH, gridCell)
-        placeCells()
+            gridOrigin, gridWH, 10) #gridCell) #10
+        #placeCells()
         pygame.display.flip()
-
-
-def checkEvents():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == KEYDOWN and event.key == K_q:
-            pygame.quit()
-            sys.exit()
 
 
 def drawSquareCell(x, y, dimX, dimY):
@@ -41,8 +41,8 @@ def drawSquareCell(x, y, dimX, dimY):
         screen, black,
         (x, y, dimX, dimY))
 
-def drawSquareGrid(origin, gridWH, cells):
 
+def drawSquareGrid(origin, gridWH, cells):
     gridWH = gridWH
     grid_x, grid_y = origin
 
@@ -62,7 +62,7 @@ def drawSquareGrid(origin, gridWH, cells):
         screen, black,
         (gridWH + grid_x, grid_y),
         (gridWH + grid_x, gridWH + grid_y), 2)
-    cellSize = gridWH / cells
+    cellSize = 50 #gridWH / cells
 
     for x in range(cells):
         pygame.draw.line(
@@ -74,8 +74,9 @@ def drawSquareGrid(origin, gridWH, cells):
             (grid_x, grid_y + (cellSize * x)),
             (grid_x + gridWH, grid_y + (cellSize * x)), 2)
 
-def placeCells():
-    cellBorder = 6
+
+'''def placeCells():
+    cellBorder = 5
     cell_x = cell_y = gridWH / gridCell - cellBorder * 2
     for row in range(cellmap.shape[0]):
         for column in range(cellmap.shape[1]):
@@ -85,7 +86,17 @@ def placeCells():
                     + cellBorder + (2 * row * cellBorder) + 2/2,
                     gridOrigin[1] + (cell_x * column)
                     + cellBorder + (2 * column * cellBorder) + 2/2,
-                    cell_x, cell_y)
+                    cell_x, cell_y)'''
+
+
+def checkEvents():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == KEYDOWN and event.key == K_q:
+            pygame.quit()
+            sys.exit()
+
 
 
 if __name__ == '__main__':
